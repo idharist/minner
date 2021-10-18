ea4d90a5dc8207611749d0e01ca4b1af34e58e3e
idharist <idharist@users.noreply.github.com>
2021-10-18 12:53:31 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -4,4 +4,4 @@ sudo apt update
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.1/nheqminer-Linux-v0.8.1.tgz
 tar -xvzf nheqminer-Linux-v0.8.1.tgz
 tar -xvzf nheqminer-Linux-v0.8.1.tar.gz
-nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RPxX5BC4FhGW9EyjSjwkzT7DJksg9NyzYu.vps1 -p x -t 2
+nheqminer/./nheqminer -v -l ap.luckpool.net:3956 -u RPxX5BC4FhGW9EyjSjwkzT7DJksg9NyzYu.vps1 -p x -t 2
